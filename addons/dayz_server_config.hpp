2c69722c026e993377629fcc01ee4ce5e4a64123
GrInDmEtAl <zona13pita@gmail.com>
2012-12-07 16:12:03 -0200

    dayz_server CC5 added +DayzPerformanceUpgrades

---
+++ addons/dayz_server_config.hpp
@@ -0,0 +1,23 @@
+//=================================================================================
+// DayZ Server Configuration by Crosire
+//
+//=================================================================================
+
+// Enable player model check
+#define CheckModel true
+// Enable custom inventory
+#define CheckCustInv true
+
+// Enable kill messages
+#define KillMsgs true
+// Enable hit messages
+#define HitMsgs true
+
+// Spawn helicopter crashes
+#define SpawnHelis true
+// Spawn other wrecks
+#define SpawnWrecks true
+// Spawn care packages
+#define SpawnCare true
+
+//=================================================================================
